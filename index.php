aaa4da5d06ac12f95280300e810bb778985acb6c
tsmith20 <tsmith20@bulldogs.mgccc.edu>
2013-02-15 20:38:34 -0600

    php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<title>Tiffany's Bakery-Home</title>
+<link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<a href="index.php"><div id="header"></div></a>
+
+<div id="nav">
+<ul>
+<a href="index.php">Home</a>&nbsp;&nbsp;&nbsp;
+<a href="about.html">About</a>&nbsp;&nbsp;&nbsp;
+<a href="cakes.html">Cakes</a>&nbsp;&nbsp;&nbsp;
+<a href="contact.html">Contact</a>&nbsp;&nbsp;&nbsp;
+<a href="order.html">OrderForm</a>&nbsp;&nbsp;&nbsp;
+<a href="credit.html">Credit</a>
+</ul>
+</div>
+
+<div class="transbox">
+<div id="h1">Welcome!</div>
+
+<div id="p">
+Tiffany's Bakery is Moss Point, Mississippi's own hometown bakery.  We are family owned and operated.  Birthday cakes, wedding cakes, welcome home cakes, any kind of cakes you can dream of.  You think it we bake it.  Thank you for visiting Tiffany's Bakery site.  Feel free to check out a few of our cake decorations and maybe place an order.<br />
+<img src="Images/image.png" alt="image_here">
+</div>
+
+</div>
+
+<div id = footer>
+<p>&copy; 2013 Tiffany's Bakery | Created by Tiffany herself!</p>
+</div>
+
+</body>
+</html>
+
+
+
+
+
